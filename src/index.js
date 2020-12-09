eca760082cab49426715bb73956bb577da5b2cda
anggifergian <anggyfergian56@gmail.com>
2020-12-09 16:41:31 +0700

    Redux fundamental

--- src/index.js
+++ src/index.js
@@ -1,56 +1,7 @@
-const input = { tag: 'JAVASCRIPT' }
+import store from './redux/store'
+import { bugAdded, bugResolved } from './redux/actions'
 
-function toLowerInput(input) {
-  const data = { ...input }
-  return `(${data.tag.toLowerCase()})`
-}
+store.dispatch(bugAdded('bug2'))
+store.dispatch(bugResolved(1))
 
-const updated = toLowerInput(input)
-
-// Best practices
-import { pipe } from 'lodash/fp'
-
-const pickTag = (obj) => obj.tag
-const toLowerCase = (str) => str.toLowerCase()
-const bracketify = (str) => `(${str})`
-
-const transform = pipe(pickTag, toLowerCase, bracketify)
-
-const output = transform({ tag: 'JAVASCRIPT' })
-
-/* ====================== */
-
-const recipe = {
-  name: 'Spagethi',
-  ingredients: ['egg', 'salt'],
-}
-
-const pickIngredients = (recipe) => recipe.ingredients
-const addIngredients = (ing) => (addIng) => [...addIng, ing]
-const searchIngredients = (ing) => (ings) =>
-  ings.map((data) => (data === ing ? `${data} white` : data))
-
-const transform2 = pipe(
-  pickIngredients,
-  addIngredients('cream'),
-  searchIngredients('egg')
-)
-const output2 = transform2(recipe)
-console.log(output2)
-
-/* ====================== */
-
-const addedIng = {
-  ...recipe,
-  ingredients: [...recipe.ingredients, 'cream'],
-}
-
-const updatedIng = {
-  ...recipe,
-  ingredients: [
-    ...recipe.ingredients,
-    ...recipe.ingredients.map((ing) => (ing === 'egg' ? 'egg white' : ing)),
-  ],
-}
-
-console.log(updatedIng)
+console.log(store.getState())
